0c56a64e247f8b439ec5f2948abeb1a72875656f
Victor Marczyk <marczykv@gmail.com>
2023-09-07 21:13:07 -0400

    routers created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const apiRoutes = require('./apiRoute');
 
-router.use('/api', apiRoutes); 
+router.use('/api', apiRoutes);  // Prefix all routes with '/api'
 
 module.exports = router;
--- controllers/api/routes/index.js
+++ controllers/api/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-const apiController = require('./controller/api');  // directs to 'api/index.js'
+const allRoutes = require('./routes');  
 
+app.use(allRoutes);
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
