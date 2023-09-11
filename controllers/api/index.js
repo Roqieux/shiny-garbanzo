62d594a2d6086ed49654dd643b8a7bff5c79d2be
Victor Marczyk <marczykv@gmail.com>
2023-09-11 19:39:20 -0400

    CRUD established

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const apiRoutes = require('./apiRoute');
-
-router.use('/api', apiRoutes);  // Prefix all routes with '/api'
-
-module.exports = router;
---
+++ controllers/routes/apiRoutes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (connection) => {
+    // User Management
+    router.post('/register', (req, res) => {
+       
+        res.send("User registered!");
+    });
+
+    router.post('/login', (req, res) => {
+        
+        res.send("User logged in!");
+    });
+
+    router.get('/profile', (req, res) => {
+        
+        res.json({ username: "JohnDoe", email: "john@example.com" });
+    });
+
+    router.post('/logout', (req, res) => {
+        
+        res.send("User logged out!");
+    });
+
+    // Inventory Management
+    router.post('/item', (req, res) => {
+        const newItem = req.body;
+        connection.query('INSERT INTO items SET ?', newItem, (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            res.json({ id: results.insertId, ...newItem });
+        });
+    });
+
+    router.put('/item/:itemId', (req, res) => {
+        connection.query('UPDATE items SET ? WHERE id = ?', [req.body, req.params.itemId], (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            if (results.affectedRows === 0) return res.status(404).send("Item not found");
+            res.json({ id: req.params.itemId, ...req.body });
+        });
+    });
+
+    router.delete('/item/:itemId', (req, res) => {
+        connection.query('DELETE FROM items WHERE id = ?', [req.params.itemId], (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            if (results.affectedRows === 0) return res.status(404).send("Item not found");
+            res.send("Item deleted successfully!");
+        });
+    });
+
+    router.get('/items', (req, res) => {
+        connection.query('SELECT * FROM items', (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            res.json(results);
+        });
+    });
+
+    router.get('/item/:itemId', (req, res) => {
+        connection.query('SELECT * FROM items WHERE id = ?', [req.params.itemId], (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            if (results.length === 0) return res.status(404).send("Item not found");
+            res.json(results[0]);
+        });
+    });
+
+    // Alerts
+    router.get('/alerts/expiring', (req, res) => {
+        connection.query('SELECT * FROM items WHERE daysToExpire <= 7', (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            res.json(results);
+        });
+    });
+
+    router.get('/alerts/lowstock', (req, res) => {
+        connection.query('SELECT * FROM items WHERE quantity < 5', (err, results) => {
+            if (err) return res.status(500).json({ error: 'Database error' });
+            res.json(results);
+        });
+    });
+
+    return router;
+};
