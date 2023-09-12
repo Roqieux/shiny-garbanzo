8587e2a0994fea22ec4615067b2b610ef957d403
dopalescent <skudlgre000@gmail.com>
2023-09-12 16:00:50 -0400

    testing geocoder to convert street address into coordinates for map marker placement

--- public/map.js
+++ public/map.js
@@ -6,4 +6,24 @@ async function initMap() {
     center: { lat: 39.97774274569622, lng: -75.15607170327148 },
     zoom: 12,
   });
-}
+
+  // Todo: get fridges array from db table
+  {
+    const location = {
+      id: 1,
+      fridge_name: "700 N 3rd St",
+      OwnerId: 1,
+      coords: "700 N 3rd St",
+      isPublic: true
+    };
+    const addressString = location.coords.replaceAll(' ', '+');
+
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressString},+Philadelphia,+PA&key=AIzaSyD6L6whRm8FlsozW7lN8bUic-Jh8uClIyU`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const locLatLng = data.results[0].geometry.location;
+        console.log(locLatLng)
+      })
+  };
+};
