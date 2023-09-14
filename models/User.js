3f798dc512392b4621271550f3709a83ca9c9066
dopalescent <skudlgre000@gmail.com>
2023-09-14 18:27:11 -0400

    trying to get mapRoutes to work, fixed 'flase' typo in User model, error seeding user table: field 'password' does not have a default value'

--- models/User.js
+++ models/User.js
@@ -2,50 +2,50 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class User extends Model {}
+class User extends Model { }
 
 
 User.init(
-    {
-      // define columns
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      first_name: {
-        type: DataTypes.STRING,
-
-      },
-      last_name: {
-        type: DataTypes.STRING,
-
-      },
-      username: {
-        type: DataTypes.STRING,
-        unique: true,
-        allownull: flase
-
-      },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false
-
-      },
-      Password: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-
-      },
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user',
-    }
-  
-  );
-  module.exports = User
+    first_name: {
+      type: DataTypes.STRING,
+
+    },
+    last_name: {
+      type: DataTypes.STRING,
+
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false
+
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+
+    },
+    Password: {
+      type: DataTypes.STRING,
+      allowNull: false
+
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+
+);
+module.exports = User
