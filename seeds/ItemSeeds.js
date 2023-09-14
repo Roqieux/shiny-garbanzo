8adfdb015cf0f9864decdd2df76ad13110fbc887
Julien <larroquejl@gmail.com>
2023-09-14 19:45:03 -0400

    cleaned seed data and models for real for real

--- seeds/ItemSeeds.js
+++ seeds/ItemSeeds.js
@@ -4,57 +4,57 @@ const itemSeed = [
     {
         id: 1,
         item_name: "Tomato",
-        UserId: 1,
-        FridgeId: 1,
-        Expiration_date: "11/12/2023",
+        user_id: 1,
+        fridge_id: 1,
+        expiration_date: "11/12/2023",
         is_frozen: false
     },
     {
         id: 2,
         item_name: "Tomato",
-        UserId: 1,
-        FridgeId: 1,
-        Expiration_date: "11/03/2023",
+        user_id: 1,
+        fridge_id: 1,
+        expiration_date: "11/03/2023",
         is_frozen: true
     },
     {
         id: 3,
         item_name: "Spinich",
-        UserId: 1,
-        FridgeId: 1,
-        Expiration_date: "10/12/2023",
+        user_id: 1,
+        fridge_id: 1,
+        expiration_date: "10/12/2023",
         is_frozen: false
     },
     {
         id: 4,
         item_name: "Apples",
-        UserId: 1,
-        FridgeId: 1,
-        Expiration_date: "11/20/2023",
+        user_id: 1,
+        fridge_id: 1,
+        expiration_date: "11/20/2023",
         is_frozen: false
     },
     {
         id: 5,
         item_name: "Beef",
-        UserId: 1,
-        FridgeId: 2,
-        Expiration_date: "9/20/2023",
+        user_id: 1,
+        fridge_id: 2,
+        expiration_date: "9/20/2023",
         is_frozen: false
     },
     {
         id: 6,
         item_name: "Bacon",
-        UserId: 1,
-        FridgeId: 2,
-        Expiration_date: "9/29/2023",
+        user_id: 1,
+        fridge_id: 2,
+        expiration_date: "9/29/2023",
         is_frozen: true
     },
     {
         id: 7,
         item_name: "Tomato",
-        UserId: 1,
-        FridgeId: 2,
-        Expiration_date: "11/12/2023",
+        user_id: 1,
+        fridge_id: 2,
+        expiration_date: "11/12/2023",
         is_frozen: false
     },
 ]
--- seeds/UserSeeds.js
+++ seeds/UserSeeds.js
@@ -6,7 +6,7 @@ const userSeed = [
         first_name: "Elizabeth",
         last_name: "Quinn",
         username: "Brou",
-        email: "Smething@something.com",
+        email: "Smething1@something.com",
         password: "BlahBlah"
     },
     {
@@ -14,7 +14,7 @@ const userSeed = [
         first_name: "Jude",
         last_name: "Hartmann",
         username: "Dbear",
-        email: "Smething@something.com",
+        email: "Smething2@something.com",
         password: "BlahBlah"
     },
     {
@@ -22,7 +22,7 @@ const userSeed = [
         first_name: "Victor",
         last_name: "Marczyk",
         username: "Vic",
-        email: "Smething@something.com",
+        email: "Smething3@something.com",
         password: "BlahBlah"
     },
     {
@@ -30,7 +30,7 @@ const userSeed = [
         first_name: "Julien",
         last_name: "Larroque",
         username: "Croissant",
-        email: "Smething@something.com",
+        email: "Smething4@something.com",
         password: "BlahBlah"
     },
     {
@@ -38,7 +38,7 @@ const userSeed = [
         first_name: "Greg",
         last_name: "Skudlarek",
         username: "Greg",
-        email: "Smething@something.com",
+        email: "Smething5@something.com",
         password: "BlahBlah"
     },
 ]
